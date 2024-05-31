33196d49f9375d56d7d135395331bd73960aa536
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2024-05-31 16:11:38 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/root', function () {
     return view('welcome');
 });
 
-Route::get('/portfolio', [LandingPageController::class, 'index']);
+Route::get('/', [LandingPageController::class, 'index']);
